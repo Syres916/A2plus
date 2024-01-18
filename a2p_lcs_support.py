46bfae721ac0adb888fdcb3305b118e583d6306c
Syres916 <46537884+Syres916@users.noreply.github.com>
2024-01-18 11:25:57 +0000

    [A2P] Python 3.11.x and above Json Fix for FreeCAD 0.21.2 and above

--- a2p_lcs_support.py
+++ a2p_lcs_support.py
@@ -91,6 +91,12 @@ class VP_LCS_Group(object):
     def __setstate__(self,state):
         return None
 
+    def dumps(self):
+        return None
+
+    def loads(self,state):
+        return None
+
 #==============================================================================
 
 def getListOfLCS(targetDoc,sourceDoc):
